aabdf81365c811811a2931c22f3c3aad6d4b1ce5
gourav singh <gsingh6122016@gmail.com>
2020-07-16 23:22:30 +0530

    new change

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import requests
 import hashlib
 import sys
+import random
+from tkinter import *
+from tkinter import messagebox ,Menu
+
+password_check = Tk()
+password_check.title("Check Your password")
+
 
 def request_api_data(first_char):
     url = 'https://api.pwnedpasswords.com/range/' + first_char
@@ -23,13 +30,66 @@ def pwned_api_check(password):
     response = request_api_data(first5_char)
     return get_password_leak_count(response ,tail)
 
-def main(args) :
-    for password in args :
-        count = pwned_api_check(password)
-        if count :
-            print(f'{password} was FOUND {count} times... you should change your password')
-        else:
-            print(f'{password} was NOT FOUND . carry on !')
-    return 'Program Complete!'
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+def generator(password) :
+    digits = "0123456789!@#$%^&*()"
+    new_password=""
+    for x in password :
+        if not x.isdigit() :
+            new_password = new_password + x
+    if len(new_password) > 6 :
+        new_password = new_password[:6]
+
+    length = 10 - len(new_password)
+    count = 1
+    for i in range(0, length):
+        new_password = new_password + random.choice(digits)
+    count = pwned_api_check(new_password)
+    while(count):
+        new_password = new_password[:(len(new_password) - length)]
+        for i in range(0, length):
+            new_password = new_password + random.choice(digits)
+        count = pwned_api_check(new_password)
+
+    return new_password
+
+
+# def main(args) :
+#     for password in args :
+#         count = pwned_api_check(password)
+#         if count :
+#             print(f'{password} was FOUND {count} times... you should change your password')
+#             result = generator(password)
+#             print(f'You can try {result}')
+#         else:
+#             print(f'{password} was NOT FOUND . carry on !')
+#     return 'Program Complete!'
+#
+# if __name__ == '__main__':
+#     sys.exit(main(sys.argv[1:]))
+
+
+def main():
+    count = pwned_api_check(enter_password.get())
+    if count :
+        result = generator(enter_password.get())
+        messagebox.showinfo("Notification" ,f'{enter_password.get()} was FOUND {count} times...    You can try {result}')
+    else:
+        messagebox.showinfo("Notification" ,f'{enter_password.get()} was NOT FOUND . carry on !')
+
+password_text = Label(password_check ,text = "  Enter Your password  " ,bg = "white" ,fg = "black" ,font = "Lato 12 bold" ,padx = "2" ,pady = "2" ,borderwidth = 2 ,relief = "groove" )
+password_text.grid(row = 0 ,column = 0,sticky = N+W+E+S ,padx = 5, pady = 5)
+
+enter_password = Entry(password_check , borderwidth = 2 ,relief = "groove" ,width = 30 )
+enter_password.grid(row = 0 ,column = 1,sticky = N+W+E+S ,padx = 5, pady = 5)
+
+submit_button= Button(password_check ,text = "Submit" ,command = main  ,bg = "grey" ,fg = "blue" ,font = "Lato 12 bold" ,padx = "2" ,pady = "2" ,borderwidth = 2 ,relief = "groove"  )
+submit_button.grid(row = 1 ,column = 0 , columnspan = 2 ,padx = 5, pady = 5)
+
+
+# password_text = Label(password_check ,text = f'{password} was FOUND {count} times...' ,bg = "white" ,fg = "black" ,font = "Lato 12 bold" ,padx = "2" ,pady = "2" ,borderwidth = 2 ,relief = "groove")
+# password_text.grid(row = 2 ,column = 0 ,columnspan = 2,sticky = N+W+E+S ,padx = 5, pady = 5)
+#
+# text = Label(password_check ,text = f'You can try {result}' ,bg = "white" ,fg = "black" ,font = "Lato 12 bold" ,padx = "2" ,pady = "2" ,borderwidth = 2 ,relief = "groove")
+# text.grid(row = 3 ,column = 0 ,columnspan = 2,sticky = N+W+E+S ,padx = 5, pady = 5)
+
+password_check.mainloop()
